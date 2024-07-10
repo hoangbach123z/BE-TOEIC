1ff83f34451e2ecc435f6c7448dd754c75a0515c
Bach Hoang <bach.nh@smartmind.vn>
2024-07-10 14:53:15 +0700

    init

---
+++ src/main/java/com/backend/spring/BeToeicApplication.java
@@ -0,0 +1,13 @@
+package com.backend.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeToeicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeToeicApplication.class, args);
+    }
+
+}
